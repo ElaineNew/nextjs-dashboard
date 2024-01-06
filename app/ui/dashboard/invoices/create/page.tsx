8d762fdbde4073bcf409b345f21d3dca99365d06
ElaineNew <elaineqiu0531@gmail.com>
2024-01-05 20:29:55 -0500

    finish invoice

---
+++ app/ui/dashboard/invoices/create/page.tsx
@@ -0,0 +1,28 @@
+import Form from '@/app/ui/dashboard/invoices/create-form';
+import Breadcrumbs from '@/app/ui/dashboard/invoices/breadcrumbs';
+import { fetchCustomers } from '@/app/lib/data';
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: 'Create Invoice',
+};
+
+export default async function Page() {
+  const customers = await fetchCustomers();
+ 
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: 'Invoices', href: '/ui/dashboard/invoices' },
+          {
+            label: 'Create Invoice',
+            href: '/ui/dashboard/invoices/create',
+            active: true,
+          },
+        ]}
+      />
+      <Form customers={customers} />
+    </main>
+  );
+}
